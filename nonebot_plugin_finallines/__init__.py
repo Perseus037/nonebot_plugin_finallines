dfb3a2855a38bba0a1c6949cc7319df7e94e19f5
黑纸折扇 <143330850+Perseus037@users.noreply.github.com>
2024-01-13 07:33:34 +0800

    支持多平台适配

--- nonebot_plugin_finallines/__init__.py
+++ nonebot_plugin_finallines/__init__.py
@@ -3,18 +3,20 @@ import json
 import os
 
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.adapters import Bot, Event
+from nonebot_plugin_saa import MessageFactory, Text
+from nonebot_plugin_userinfo import UserInfo, EventUserInfo
 from nonebot.plugin import PluginMetadata
 
-__version__ = "0.1.0.post1"
+__version__ = "0.1.0.post2"
 __plugin_meta__ = PluginMetadata(
     name="最终台词",
-    description="来一句劲道的最终台词吧",
+    description="来一句劲道的最终台词吧,支持多平台适配",
     usage="使用命令：最终台词",
     homepage="https://github.com/Perseus037/nonebot_plugin_finallines",
     type="application",
     config=None,
-    supported_adapters={"~onebot.v11"},
+    supported_adapters={"~onebot.v11", "~onebot.v12", "~QQ Guild",  "WeChat", "~Kaiheila", "Telegram", "Feishu","Red"},
 )
 
 # 读取json文件
@@ -33,13 +35,10 @@ final_words = load_final_lines()
 final_words_cmd = on_command("最终台词", priority=1)
 
 @final_words_cmd.handle()
-async def handle_first_receive(bot: Bot, event: Event):
-    user_id = int(event.get_user_id())
-    user_info = await bot.get_stranger_info(user_id=user_id)
-    nickname = user_info.get("nickname", "你")
-
-    # 从台词列表中随机选择一句并发送
+async def handle(user_info: UserInfo = EventUserInfo()):
+    nickname = user_info.user_name if user_info.user_name else "你"
     final_word = random.choice(final_words)
     reply = f"{nickname}的最终台词是：{final_word}"
 
-    await final_words_cmd.finish(reply)
+    message_builder = MessageFactory([Text(reply)])
+    await message_builder.send()
