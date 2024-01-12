b7dece34dff7ec6e4e733df739b42e534ae93255
黑纸折扇 <143330850+Perseus037@users.noreply.github.com>
2024-01-12 16:50:23 +0800

    Update __init__.py

--- nonebot_plugin_finallines/__init__.py
+++ nonebot_plugin_finallines/__init__.py
@@ -4,6 +4,18 @@ import os
 
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.plugin import PluginMetadata
+
+__version__ = "0.1.0.post1"
+__plugin_meta__ = PluginMetadata(
+    name="最终台词",
+    description="来一句劲道的最终台词吧",
+    usage="使用命令：最终台词",
+    homepage="https://github.com/Perseus037/nonebot_plugin_finallines",
+    type="application",
+    config=None,
+    supported_adapters={"~onebot.v11"},
+)
 
 # 读取json文件
 def load_final_lines():
@@ -22,7 +34,6 @@ final_words_cmd = on_command("最终台词", priority=1)
 
 @final_words_cmd.handle()
 async def handle_first_receive(bot: Bot, event: Event):
-    # 获取触发命令的用户的QQ号，并转换为整数
     user_id = int(event.get_user_id())
     user_info = await bot.get_stranger_info(user_id=user_id)
     nickname = user_info.get("nickname", "你")
